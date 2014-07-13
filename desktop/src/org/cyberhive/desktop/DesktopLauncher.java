4a7d5e0ff92eedae62ce4fc0aa92d934ed746d2d
Vladimir Zarypov <krre31@gmail.com>
2014-07-13 14:13:24 +0300

    initial commit

---
+++ desktop/src/org/cyberhive/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.cyberhive.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.cyberhive.CyberHive;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CyberHive(), config);
+	}
+}
