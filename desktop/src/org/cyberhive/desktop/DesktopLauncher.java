501fa4da2937d50f99b82cc8419f4308d4306277
Vladimir Zarypov <krre31@gmail.com>
2014-07-17 11:53:57 +0300

    desktop resolution setted to 1280x720

--- desktop/src/org/cyberhive/desktop/DesktopLauncher.java
+++ desktop/src/org/cyberhive/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import org.cyberhive.CyberHive;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1280;
+        config.height = 720;
 		new LwjglApplication(new CyberHive(), config);
 	}
 }
