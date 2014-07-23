a27b2a966c70e6145d657eafd387e496bb8a4394
Vladimir Zarypov <krre31@gmail.com>
2014-07-23 20:22:57 +0300

    hexagon texture is displayable

--- src/core/src/org/cyberhive/screen/GameScreen.java
+++ src/core/src/org/cyberhive/screen/GameScreen.java
@@ -4,17 +4,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import org.cyberhive.CyberHive;
 
 public class GameScreen implements Screen {
     CyberHive game;
     OrthographicCamera camera;
+    Texture hexture;
 
 
     public GameScreen(final CyberHive game) {
         this.game = game;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, CyberHive.VIRTUAL_WIDTH, CyberHive.VIRTUAL_HEIGHT);
+
+        hexture = new Texture(Gdx.files.internal("hexagon.png"));
     }
 
     @Override
@@ -26,7 +30,7 @@ public class GameScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch, "Welcome to CyberHive! ", 100, 150);
+        game.batch.draw(hexture, 0, 0);
         game.batch.end();
     }
 
