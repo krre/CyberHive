fdee8cba44c242ef2cf915e21769a8e37c3ba330
Vladimir Zarypov <krre31@gmail.com>
2014-07-30 11:23:54 +0300

    created hexagon map

--- src/core/src/org/cyberhive/screen/GameScreen.java
+++ src/core/src/org/cyberhive/screen/GameScreen.java
@@ -5,12 +5,21 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import org.cyberhive.CyberHive;
 
 public class GameScreen implements Screen {
     CyberHive game;
     OrthographicCamera camera;
+    TiledMap map;
     Texture hexture;
+    HexagonalTiledMapRenderer renderer;
 
 
     public GameScreen(final CyberHive game) {
@@ -19,6 +28,26 @@ public class GameScreen implements Screen {
         camera.setToOrtho(false, CyberHive.VIRTUAL_WIDTH, CyberHive.VIRTUAL_HEIGHT);
 
         hexture = new Texture(Gdx.files.internal("hexagon.png"));
+        TextureRegion[][] hexes = TextureRegion.split(hexture, 103, 90);
+        map = new TiledMap();
+        MapLayers layers = map.getLayers();
+        TiledMapTile[] tiles = new TiledMapTile[4];
+        tiles[0] = new StaticTiledMapTile(new TextureRegion(hexes[0][0]));
+        tiles[1] = new StaticTiledMapTile(new TextureRegion(hexes[0][1]));
+        tiles[2] = new StaticTiledMapTile(new TextureRegion(hexes[1][0]));
+        tiles[3] = new StaticTiledMapTile(new TextureRegion(hexes[1][1]));
+
+        TiledMapTileLayer layer = new TiledMapTileLayer(45, 30, 103, 90);
+        for (int y = 0; y < 30; y++) {
+            for (int x = 0; x < 45; x++) {
+                TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
+                cell.setTile(tiles[0]);
+                layer.setCell(x, y, cell);
+            }
+        }
+        layers.add(layer);
+        renderer = new HexagonalTiledMapRenderer(map);
+
     }
 
     @Override
@@ -27,11 +56,8 @@ public class GameScreen implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
-
-        game.batch.begin();
-        game.batch.draw(hexture, 0, 0);
-        game.batch.end();
+        renderer.setView(camera);
+        renderer.render();
     }
 
     @Override
