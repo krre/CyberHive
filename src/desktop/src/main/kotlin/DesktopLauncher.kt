18cb2d02ab62ce46ecce5de08be0c9bfd74b0f98
Vladimir Zarypov <krre31@gmail.com>
2014-08-01 19:41:16 +0300

    desktop module ported on kotlin

---
+++ src/desktop/src/main/kotlin/DesktopLauncher.kt
@@ -0,0 +1,12 @@
+package org.cyberhive.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import org.cyberhive.CyberHive
+
+public fun main(arg: Array<String>) {
+    val config = LwjglApplicationConfiguration()
+    config.width = 800
+    config.height = 480
+    LwjglApplication(CyberHive(), config)
+}
