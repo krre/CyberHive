38a4d9748e6703a72d6cf76b86ddf7c69d9ff64b
Vladimir Zarypov <krre31@gmail.com>
2014-08-02 09:33:30 +0300

    fail attempt to port android module to kotlin

---
+++ src/android/src/main/kotlin/AndroidLauncher.kt
@@ -0,0 +1,27 @@
+package org.cyberhive.android
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import org.cyberhive.CyberHive
+import android.content.Intent
+/*
+public class AndroidLauncher() : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        config.useAccelerometer = false
+        config.useCompass = false
+        initialize(CyberHive(), config)
+    }
+
+    override fun startActivity(intent: Intent?) {
+    }
+
+    override public fun runOnUiThread(runnable: Runnable?) {
+
+    }
+
+}
+*/
