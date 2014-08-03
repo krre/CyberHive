22ad00f77b0a0ea5bc28bbd112d31d39436977ea
Vladimir Zarypov <krre31@gmail.com>
2014-08-03 17:12:27 +0300

    added NEXT button on bottom bar

--- src/core/src/main/kotlin/gui/BottomBar.kt
+++ src/core/src/main/kotlin/gui/BottomBar.kt
@@ -1,9 +1,13 @@
 package org.cyberhive.gui
 
-import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup
 import org.cyberhive.CyberHive
 import com.badlogic.gdx.scenes.scene2d.ui.Stack
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.scenes.scene2d.InputEvent
 
 public class BottomBar() : Stack() {
     {
@@ -13,5 +17,14 @@ public class BottomBar() : Stack() {
         colorBar.setSize(getWidth(), getHeight())
         colorBar.fillColor = Color.TEAL
         addActor(colorBar)
+
+        val skin = Skin(Gdx.files?.internal("uiskin.json"));
+        val nextButton = TextButton("NEXT", skin)
+        nextButton.addListener(object: ClickListener() {
+            override fun clicked(enent: InputEvent?, x: Float, y: Float) {
+                CyberHive.print("next")
+            }
+        });
+        addActor(nextButton)
     }
 }
