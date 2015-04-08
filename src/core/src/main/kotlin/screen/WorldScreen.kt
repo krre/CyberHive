f1944af4f755181e3dcb53051465283e4adabdfc
Vladimir Zarypov <krre31@gmail.com>
2015-04-08 13:04:12 +0300

    added selectors for world properties

--- src/core/src/main/kotlin/screen/WorldScreen.kt
+++ src/core/src/main/kotlin/screen/WorldScreen.kt
@@ -1,6 +1,8 @@
 package org.cyberhive.screen
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
@@ -17,8 +19,31 @@ class WorldScreen : AbstractScreen() {
         var buttonsTable = Table()
 
         mainTable.add(settingsTable).width(550f)
-        mainTable.add(buttonsTable).width(250f)
+        settingsTable.defaults().height(50f).space(10f)
 
+        val sizeLabel = Label("Size:", skin)
+        settingsTable.add(sizeLabel)
+        val sizeSelectBox = SelectBox<String>(skin)
+        sizeSelectBox.setItems("Small", "Normal", "Large")
+        settingsTable.add(sizeSelectBox)
+        settingsTable.row()
+
+        val climateLabel = Label("Climate:", skin)
+        settingsTable.add(climateLabel)
+        val climateSelectBox = SelectBox<String>(skin)
+        climateSelectBox.setItems("Cold", "Normal", "Hot")
+        settingsTable.add(climateSelectBox)
+        settingsTable.row()
+
+        val geoLabel = Label("Geo:", skin)
+        settingsTable.add(geoLabel)
+        val geoSelectBox = SelectBox<String>(skin)
+        geoSelectBox.setItems("Island", "Coast", "Сontinent")
+        settingsTable.add(geoSelectBox)
+        settingsTable.row()
+
+
+        mainTable.add(buttonsTable).width(250f)
         buttonsTable.defaults().width(200f).height(50f).space(10f)
 
         val randomWorldButton = TextButton("Random World", skin)
