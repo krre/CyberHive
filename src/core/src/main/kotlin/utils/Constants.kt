9054c1f019d358573b1cae718904719c3baeb474
Vladimir Zarypov <krre31@gmail.com>
2015-04-08 19:20:53 +0300

    converted Constants class to singleton object

--- src/core/src/main/kotlin/utils/Constants.kt
+++ src/core/src/main/kotlin/utils/Constants.kt
@@ -1,11 +1,8 @@
 package org.cyberhive.utils
 
-class Constants {
-
-    companion object {
-        val virtualWidth = 800f
-        val virtualHeight = 480f
-    }
+object Constants {
+    val virtualWidth = 800f
+    val virtualHeight = 480f
 }
 
 
